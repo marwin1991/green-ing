99e81511f9378086cfaa11a5b2bdcf6d4ae8a4d8
Peter <peter.zmilczak@gmail.com>
2023-04-17 20:34:31 +0200

    created repo and base aspecj project

---
+++ src/test/java/io/github/marwin1991/greening/WebAppTest.java
@@ -0,0 +1,10 @@
+package io.github.marwin1991.greening;
+
+import org.junit.jupiter.api.Test;
+
+public class WebAppTest {
+    @Test
+    public void testInjector() {
+        new WebApp().testInjector();
+    }
+}
